67fddbcc53906b7c498df0019aea3b8ff84ba286
Pooya Dehghani <pooya.dehghani@hm.com>
2022-12-01 14:15:15 +0100

    Init font

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../assets/styles/main.scss";
+import { Josefin_Sans } from "@next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html,
+        body {
+          font-family: ${josefin.style.fontFamily};
+        }
+      `}</style>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
