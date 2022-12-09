cce031b405dfd3200c0efc1cbe598347e7093ff8
pooyadhgh <pooya.dhgh@gmail.com>
2022-12-09 16:47:34 +0100

    Modify content

--- constants/portfolio.js
+++ constants/portfolio.js
@@ -2,7 +2,7 @@ const PORTFOLIO = [
   {
     title: "Soltia Website",
     description:
-      "Soltia.com  is an IT consultant which works with big companies in their customer portfolio. So for expanding their company, they need to have a user-friendly website to be well-known along the word.",
+      "Soltia.com  is an IT consultant that works with big companies in their customer portfolio. For expanding their company, they need to have a user-friendly website to be well-known along the world.",
     content: `
     <ul>
     <li>Collaborated closely with stakeholders and different product teams to design the experience and the interface of the product.</li> 
@@ -17,9 +17,9 @@ const PORTFOLIO = [
     id: "soltia-website",
   },
   {
-    title: "Soltia One",
+    title: "Soltia One Dashboard",
     description:
-      "Soltia one is a dashboard in which every employee should enter information related to the working or leave hours, invoices, ... ",
+      "Soltia one is a dashboard in which every employee should enter information related to the working or leave hours, invoices, and ... ",
     content: `
     <ul>
     <li>Collaborated closely with stakeholders and different product teams to design the experience and the interface of the product.</li> 
@@ -35,7 +35,7 @@ const PORTFOLIO = [
     id: "soltia-one",
   },
   {
-    title: "Write Me",
+    title: "Write Me Website",
     description:
       "WriteMe is a Translation service provider startup working in language industry.",
     content: `
@@ -53,14 +53,14 @@ const PORTFOLIO = [
     id: "writeme",
   },
   {
-    title: "E-commerce Application",
+    title: "E-commerce Mobile Application",
     description:
       "This is an e-commerce app for mobile version with the possibility of searching based on your desire and getting know other people’s reviews.",
     content: `<ul>
-    <li>Doing all process such as: card sorting, competitive analysis, A/B testing</li>
-    <li>Designing home page which includes different parts such as classification of products, user’s interest, ...</li>
-    <li>Designing checkout process.</li>
-    <li>Designing the search bar.</li>
+    <li>All process such as card sorting, competitive analysis, A/B testing, and ... were done.</li>
+    <li>Designed the home page which includes different parts such as classification of products, user’s interest, and ...</li>
+    <li>Designed the checkout process.</li>
+    <li>Designed the search bar.</li>
     </ul>
       `,
     figma: "https://www.figma.com/file/oGgTXoYP0hk6K1QU6bySNt/electronic",
@@ -69,16 +69,16 @@ const PORTFOLIO = [
     id: "e-commerce-application",
   },
   {
-    title: "Mitchell’s ice-cream website",
+    title: "Mitchell’s ice cream Website",
     description:
-      "This is e-commerce website which offers ice-cream and other deserts as well in different flavors based on users'  choices, and prepare this opportunity for them to order every Mitchell’s products online. ",
-    // content: `<ul>
-    // <li>Doing all process such as: card sorting, competitive analysis, A/B testing</li>
-    // <li>Designing home page which includes different parts such as classification of products, user’s interest, ...</li>
-    // <li>Designing checkout process.</li>
-    // <li>Designing the search bar.</li>
-    // </ul>
-    //   `,
+      "This is e-commerce website which offers ice cream and other deserts as well in different flavors based on users'  choices, and prepare this opportunity for them to order every Mitchell’s products online. ",
+    content: `<ul>
+    <li>Researched the users demands, and iterated again according to them.</li>
+    <li>Defined a persona, map journey and user flow.</li>
+    <li>Designed a home page in a way that user see all products at a first glance.</li>
+    <li>Designed the pages related to the ice cream ordering in a way that customers have a chance to customize their order.</li> 
+    </ul>
+      `,
     figma: "https://www.figma.com/proto/PyD3dGIRhFext7g0phfbea/working",
     year: "2022",
     image: "/images/portfolio/ice-cream.png",
